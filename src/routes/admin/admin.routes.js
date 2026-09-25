0d2f35bc1f8f95e830afd72d773c5006549c1011
MANHSELU <manhselu@gmail.com>
2026-09-25 15:14:44 +0700

    feat(feature/adminFeature): implement create, update, view, search user function

--- src/routes/admin/admin.routes.js
+++ src/routes/admin/admin.routes.js
@@ -3,6 +3,7 @@ const route = express.Router();
 const itServiceManagementController = require("../../controllers/admin/itServiceManagement.controller");
 const ticketCategoryManagementController = require("../../controllers/admin/ticketCategory.controller");
 const supportTeamManagementController = require("../../controllers/admin/supportTeamManagement.controller");
+const userManagementController = require("../../controllers/admin/userManagement.controller");
 
 // IT Services
 route.post("/services", itServiceManagementController.createItServiceController);
@@ -21,5 +22,11 @@ route.post("/support-teams", supportTeamManagementController.createSupportTeamCo
 route.get("/support-teams", supportTeamManagementController.findSupportTeamController);
 route.get("/support-teams/search", supportTeamManagementController.searchSupportTeamController);
 route.patch("/support-teams/:teamId", supportTeamManagementController.updateSupportTeamController);
+
+// Users
+route.post("/users", userManagementController.createUserController);
+route.get("/users", userManagementController.findUserController);
+route.get("/users/search", userManagementController.searchUserController);
+route.patch("/users/:userId/status", userManagementController.updateUserStatusController);
 
 module.exports = route;
